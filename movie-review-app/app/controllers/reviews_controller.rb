c89953c28af7e79c588c89779e9b75295e09331a
Noah Eldreth <neldreth@uncc.edu>
2018-11-15 07:07:31 +0000

    Added images to homepage, logo to menu, and remade review controller

--- movie-review-app/app/controllers/reviews_controller.rb
+++ movie-review-app/app/controllers/reviews_controller.rb
@@ -1,8 +1,28 @@
 class ReviewsController < ApplicationController
+    #def create
+        #@user = User.find(params[:user_id])
+       # @review = @user.reviews.create(review_params)
+       # redirect_to user_path(@user)
+   # end
+    
+    def new
+       @review = Review.new 
+    end
+    
     def create
-        @user = User.find(params[:user_id])
-        @review = @user.reviews.create(review_params)
-        redirect_to user_path(@user)
+        @review = Review.new(review_params)
+        #byebug
+        #Movie.each do |movie|
+            #if(movie.title == @review.title)
+                #@review.movie_id = movie.id
+            #end
+        #end
+        if @review.save
+            #byebug
+            redirect_to '/users'
+        else
+            redirect_to '/users'
+        end
     end
     
     private
