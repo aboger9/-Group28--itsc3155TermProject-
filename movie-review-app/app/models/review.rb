e44dca7c3d1016acac4293a4191115b5f20ca907
Noah Eldreth <neldreth@uncc.edu>
2018-12-12 05:32:13 +0000

    Final changes for presentation

--- movie-review-app/app/models/review.rb
+++ movie-review-app/app/models/review.rb
@@ -6,5 +6,5 @@ class Review < ApplicationRecord
   validates :body, presence: true, length: { minimum: 5 }
   validates :rating, presence: true, inclusion: { in: 0..10 }
   VALID_DATE_REGEX =  /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
-  validates :date, presence: true, format: { with: VALID_DATE_REGEX, :multiline => true } 
+  validates :date, presence: true, format: { with: VALID_DATE_REGEX, :multiline => true }
 end
